f6489c08bc2d5bcf3d8dc6da1da9640493e8076d
BobbyKh <bgoogly0@gmail.com>
2024-11-26 17:51:58 +0545

    logindesign

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -40,13 +40,13 @@ const Navbar = () => {
   };
 
   return (
-    <header className="bg-black dark:bg-[#d5420b] flex justify-between items-center py-6 px-10 shadow-sm">
+    <header className="bg-black flex justify-between items-center py-6 px-10 shadow-sm">
       <div className="flex items-center">
         <Link to="/">
           <img src={organization.logo} alt="ResuMaster Logo" className="w-10 h-10 md:w-12 md:h-12 mr-2" />
         </Link>
         <h1 className="text-2xl font-bold text-[#d5420b]">
-          {organization.name} Resu<span className="text-white">master</span>
+           Resu<span className="text-white">master</span>
         </h1>
       </div>
       <button onClick={toggleMenu} className="text-white md:hidden flex items-center justify-center w-10 h-10 md:w-auto md:h-auto">
@@ -55,7 +55,7 @@ const Navbar = () => {
       <nav className={`md:flex justify-center flex-grow ${isOpen ? "block" : "hidden"}`}>
         <ul className="flex flex-col md:flex-row md:space-x-6 text-white md:items-center mx-auto">
           <div className="flex items-center gap-4">
-            <Dropdown label="Tools" size="sm" color="black">
+            <Dropdown label="Tools" size="md" color="black">
               <Dropdown.Item>
                 <Link to="/resume">Resume</Link>
               </Dropdown.Item>
