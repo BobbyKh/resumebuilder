114028dad1f3d36c9f444871a63b1428cec8ef26
bobby <bgoogly0@gmail.com>
2024-12-13 01:20:16 +0545

    auth-failed-again

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-uu4^)vqameolr738s=(i8wr6pfved2=u+mt*55jbynp-xjpz(6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['resumaven.net']
 
 
 # Application definition
