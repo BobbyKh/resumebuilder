4374571be4756145ab7da679cc715a1fbdb90111
bobby <bgoogly0@gmail.com>
2024-12-12 11:37:01 +0545

    render-deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
+    chunkSizeWarningLimit: 2000, // Set to 2000 KB or an appropriate limit
     rollupOptions: {
       output: {
         manualChunks: {
-          vendor: ['react', 'react-dom'], // Separate React into a vendor chunk
-          modal: ['react-modal'],        // Separate react-modal into its own chunk
+          vendor: ['react', 'react-dom'], // Example of chunking React and ReactDOM
         },
       },
     },
