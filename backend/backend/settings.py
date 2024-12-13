148a01efd5ce9781752f7675ac58f527885a9d97
bobby <bgoogly0@gmail.com>
2024-12-13 23:52:26 +0545

    deploytest

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -192,7 +192,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import os 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
 
 CORS_ALLOWED_ORIGINS = [
     "http://resumaven.net",
